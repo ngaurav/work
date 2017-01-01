8863e3c987a162ee7367cae647fd97850f8099ed
Ubuntu <ubuntu@ip-172-31-22-124.us-west-2.compute.internal>
2017-01-01 06:21:41 +0000

    settings

--- react/settings.py
+++ react/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'react.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(os.path.join(BASE_DIR, 'react'), 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(os.path.join(BASE_DIR, 'react'), 'static'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
