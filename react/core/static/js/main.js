547045b8401448bf87c8084f24e01d2d3cb99280
Nishant Gaurav <nishantgaurav@icloud.com>
2017-01-07 03:49:43 +0530

    game 9.8

--- react/core/static/js/main.js
+++ react/core/static/js/main.js
@@ -24,99 +24,22 @@ function add_guess() {
   var guesses = $('#g-guesses');
   var guess = $('#g-guess');
   var clone = guess.clone();
+
   var e = clone.find(".go-to-btn")[0];
   e.innerHTML = inputData[0];
   e.nextElementSibling.innerHTML = inputData[1];
   e.nextElementSibling.nextElementSibling.innerHTML = inputData[2];
 
+  $('#g-input').find(".g-instruction-hed").innerHTML = "You can test another sequence if you want:";
+
+  $('.g-guess-hed')..css("display", "block");
+
   var rightWrong = (inputData[0] < inputData[1]) & (inputData[1] < inputData[2]) ? right : wrong;
   clone.find(".g-answer").remove();
   clone.append(rightWrong);
+
   guesses.append(clone);
 }
-
-function duplicate() {
-
-        var oldDiv = $('#g-input');
-
-        oldDiv.find(".g-next-probem-prompt").remove();
-
-        var clone = oldDiv.clone();
-
-        oldDiv.find('.g-check').remove();
-
-        var rightWrong = (inputData[0] < inputData[1]) & (inputData[1] < inputData[2]) ? right : wrong;
-
-        var rightWrongClass = (inputData[0] < inputData[1]) & (inputData[1] < inputData[2]) ? "g-yes-num-group" : "g-wrong-num-group";
-
-        // pic.attr("src", NYTG_ASSETS2 + "neutral.gif");
-
-
-        // if (rightWrong == right) {
-        //     pic.attr("src", NYTG_ASSETS2 + "correct.gif");
-
-        // }
-        // if (rightWrong == wrong) {
-        //     pic.attr("src", NYTG_ASSETS2 + "wrong.gif");
-
-        // }
-
-
-        oldDiv
-            .attr('id', '')
-            .attr('class', 'g-guess');
-
-        oldDiv.find('.g-numbers')
-            .append(rightWrong)
-            .addClass(rightWrongClass);
-
-        oldDiv.find("input").prop('disabled', true);
-
-        clone.find('input').val('');
-        clone.attr('class', 'g-again');
-
-        $('.g-yours').append(clone);
-        $('.g-check').prop('disabled', true);
-
-        clone.prepend('<h5 class="g-next-probem-prompt">You can test another sequence if you want:</h5>');
-
-
-        $('.g-first').focus();
-
-        // unhide box
-        $('.g-bottom-button-container').css("display", "block");
-
-        // turn on submit button
-        $('.g-submit').prop('disabled', false);
-        // $('.g-submit').css('display', 'block');
-
-        //hide the I dont want to play button
-        // $('.g-justTellMe').css("display", "none");
-
-        //change the style and text of first guess
-        $('.g-first-guess-hed').text("Your guesses:")
-            .addClass("g-first-guess-already-done");
-
-
-        // move it down a little -- desktop only.
-        if (magnum.device === "desktop") {
-            $('html,body').animate({
-               scrollTop: $(window).scrollTop() + 150
-            }, 1000);
-        }
-
-
-        // move it down a little -- desktop only.
-        if (magnum.device === "mobile") {
-            $('html,body').animate({
-               scrollTop: $(window).scrollTop() + 80
-            }, 1000);
-        }
-
-
-        dance();
-
-    }
 
 function main() {
 
