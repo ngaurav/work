1c01017c1833a42a1fcc4c0b2ffbe022e6a90429
Nishant Gaurav <nishantgaurav@icloud.com>
2017-01-08 11:54:36 +0530

    http respons

--- react/core/views.py
+++ react/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseBadRequest
 from .models import Mail
 
 # Create your views here.
